542e8a55f869328122a1415ef769dd2ebff7a3b4
ygursu <yasemingrs@gmail.com>
2024-01-27 20:12:43 +0300

    Proje dosyası ekle.

---
+++ ConsoleAppDate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
